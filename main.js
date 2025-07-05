75ab267ce75ba23eb86527eab3957292001ffc72
yusuf-lotso <yusufsywal2006@gmail.com>
2025-07-06 03:03:41 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    trings: ["Computer Science Student", "Frontend Developer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: tru
+});
